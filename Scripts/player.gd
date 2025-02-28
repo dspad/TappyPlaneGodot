b6571f7e48661f48e8d7f405e8e7e79307e73161
Daniele Spadoni <d.spadoni@infordata.net>
2025-02-28 16:19:59 +0100

    Walls

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -2,6 +2,6 @@ extends RigidBody2D
 
 @export var impulse_force : int = 1200
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("click"):
 		apply_central_impulse(Vector2.UP * impulse_force)
